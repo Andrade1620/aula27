b6eb662863ed3dcbfd5da0043cf8c899426ea7a7
Calebe Andrade <calebe.correia@aluno.senai.br>
2024-11-27 10:59:22 -0500

    Commit de calculo da media do aluno

--- aula27.py
+++ aula27.py
@@ -2,4 +2,12 @@ n1=float(input())
 n2=float(input())
 n3=float(input())
 n4= float(input())
+media= (n1+n2+n3+n4)/4
 qtd_faltas= int(input())
+qtd_alunos= int(input())
+if media >=8:
+    print("Parabéns, você foi aprovado(a)!!")
+elif media >=5:
+    print("Você falhou, e , está na recuperação!")
+elif media <5:
+    print("Infelizmente, você foi reprovado!")
