266f6108e3f5dadcea9b52874c77e89dd7ec49b9
Calebe Andrade <calebe.correia@aluno.senai.br>
2024-11-27 11:14:10 -0500

    Commit da definição de criterios

--- aula27.py
+++ aula27.py
@@ -1,13 +1,15 @@
 n1=float(input())
 n2=float(input())
 n3=float(input())
-n4= float(input())
+n4=float(input())
 media= (n1+n2+n3+n4)/4
-qtd_faltas= int(input())
-qtd_alunos= int(input())
+qtd_faltas=int(input())
 if media >=8:
     print("Parabéns, você foi aprovado(a)!!")
 elif media >=5:
-    print("Você falhou, e , está na recuperação!")
+    print("Você falhou e está na recuperação!")
 elif media <5:
     print("Infelizmente, você foi reprovado!")
+if qtd_faltas >=30:
+    print("Você foi reprovado, pela quantidade de faltas!")
+qtd_alunos= int(input())
