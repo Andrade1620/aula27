6cf1e7dc067d8ed7d62b9417007b02965c28d365
calebe <calebe.correia@aluno.senai.br>
2024-11-28 11:16:13 -0500

    commit para salvar relatório

--- aula27.py
+++ aula27.py
@@ -1,15 +1,25 @@
+qtd_aluno = int(input("Digite a quantidade de alunos:"))
+for i in range(qtd_aluno):
+    print("Digite a 1° nota:")
 n1=float(input())
+print("Digite a 2° nota:")
 n2=float(input())
+print("Digite a 3° nota:")
 n3=float(input())
+print("Digite a 4° nota:")
 n4=float(input())
 media= (n1+n2+n3+n4)/4
+print("Quantidades de faltas do aluno:")
 qtd_faltas=int(input())
 if media >=8:
     print("Parabéns, você foi aprovado(a)!!")
+    situacao= "Aprovado"
 elif media >=5:
     print("Você falhou e está na recuperação!")
 elif media <5:
     print("Infelizmente, você foi reprovado!")
 if qtd_faltas >=30:
     print("Você foi reprovado, pela quantidade de faltas!")
-qtd_alunos= int(input())
+
+    print("Nome:")
+nome= input()
