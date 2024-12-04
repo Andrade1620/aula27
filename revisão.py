27a4fb7319900bd146f6bd5e8ce97825c44942c4
Calebe Correia <calebe.correia@aluno.senai.br>
2024-12-04 10:11:16 -0500

    Novas alterações da revisão feitas às 10:10 da manhã, dia 04/12/2024

--- revisão.py
+++ revisão.py
@@ -1,32 +1,30 @@
 #LER ENTRADAS DO USUÁRIO
 cont = 0#variavel que controla a repetição
-escolha_usuario= int(input())#variavel que guarda quantas vezes o código vai rodar
+escolha_usuario= int(input("Digite a quantidade de alunos que você deseja cadastrar:"))#variavel que guarda quantas vezes o código vai rodar
+alunos = [] #lista que armazenará todos os alunos cadastrados
 while cont < escolha_usuario:
     nome= input("Digite o nome do aluno:")#ARMAZENA o nome do aluno
-    print(nome)
     nota1=float(input("Digite a primeira nota:"))#4 notas do aluno
     nota2=float(input("Digite a segunda nota:"))
     nota3=float(input("Digite a terceira nota:"))
     nota4=float(input("Digite a quarta nota:"))
 
-    faltas= int(input("Digite a quantidade de faltas do aluno:"))
+    faltas= int(input(f"Digite a quantidade de faltas o {nome} tem:"))
     #calculo da média
     media= (nota1+nota2+nota3+nota4)/4
     print(media)
     if media >=8:
-        situacao="Você está aprovado!"
+        situacao="Aprovado acima da média!"
     elif media >=5:
-        recuperacao= float(input())
+        recuperacao= float(input("digite a nota da recuperação:"))
         if recuperacao >=(10-media):
             situacao= "Aprovado na recuperação!"
         else:
             situacao= "REPROVADO na RECUPERAÇÃO!"
     else:
         situacao= "REPROVADO por média!"
+    #enviar os dados do aluno para a lista alunos
+    alunos.append([nome, faltas, media, situacao])
+    cont =+ 1
     #RELATORIO
-    print("Nome: ", nome)
-    print("Notas: ", nota1, nota2, nota3, nota4)
-    print("Faltas: ", faltas)
-    print("Media: ", media)
-    print("Situacao: ", situacao)
-cont = cont + 1
+print(alunos)
