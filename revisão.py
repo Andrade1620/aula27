b60134679690db9fa224e45ec374942d594ec7e8
Calebe <calebe.correia@aluno.senai.br>
2024-12-06 09:48:29 -0500

    Alterações feitas às 09:48 da manhã, dia 06/12/2024

--- revisão.py
+++ revisão.py
@@ -10,7 +10,7 @@ while True:
         situacao = ""
         cont = 0#variavel que controla a repetição
         escolha_usuario= int(input("Digite a quantidade de alunos que você deseja cadastrar:"))#variavel que guarda quantas vezes o código vai rodar
-        while cont < escolha_usuario:
+        while cont < escolha_usuario: 
             nome= input("Digite o nome do aluno:")#ARMAZENA o nome do aluno
             nota1=float(input("Digite a primeira nota:"))#4 notas do aluno
             nota2=float(input("Digite a segunda nota:"))
@@ -33,10 +33,13 @@ while True:
                 situacao= "REPROVADO por média!"
             #enviar os dados do aluno para a lista alunos
             alunos.append([nome, faltas, media, situacao])
-            cont =+ 1
+            cont += 1
+            print("cont:",cont)
             #RELATORIO
         
     elif escolha_menu == 2: #relatorio
         alunos.append([nome, faltas, media, situacao])
-        cont =+ 1
         print(alunos)
+        
+    elif escolha_menu == 3:#se o usuário escolheu encerrar
+        break#QUEBRA A EXECUÇÃO DO ENQUANTO
