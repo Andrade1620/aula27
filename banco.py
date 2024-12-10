0d15c133cd99ec2a045698eeb8052b6a0d35e3ce
Calebe <calebe.correia@aluno.senai.br>
2024-12-10 11:02:29 -0500

    Alterações salvas do banco às 11:02 da manhã, dia 10/12/2024

--- banco.py
+++ banco.py
@@ -1,4 +1,5 @@
 #CADASTRO DE USUÁRIO E SENHA
+saldo = 0.0 #Variável que guardará o saldo do usuário
 while True:
 #MENU PRINCIPAL
     print("Bem vindo! \n Digite 1.Cadastrar 2.Login 3.encerrar")
@@ -19,5 +20,42 @@ while True:
             print("Login realizado com sucesso!")
             #Se login correto, entra no
             #Menu principal do APP
+            print("Bem vindo! ", usuario)
+            while True:#Enquanto que exibirá o menu principal
+                print("1. Deposito 2. Sacar 3. Pix 4. Extrato 5. Encerrar")
+                escolha_principal = int(input())
+                if escolha_principal == 1:#se usuário escolher depósito 
+                    #Lê o valor a ser depositado
+                    valor_deposito = float(input("Digite o valor que você deseja depositar:"))
+                    saldo = saldo + valor_deposito #Atualiza o valor 
+                    print("Novo saldo é ", saldo)
+                elif escolha_principal == 2: #SAQUE
+                    valor_saque = float(input("Digite o valor que você deseja sacar:"))
+                    senha_saque = input("Digite sua senha: ")
+                    if senha_saque == senha:#Se senha estiver correta
+                        saldo = saldo - valor_saque #Subtrai o valor do saldo
+                        
+                    else:
+                        print("Senha incorreta!") 
+                elif escolha_principal == 3: #Se o usuário escolher pix
+                    valor_pix = float(input("Digite o valor do pix:")) 
+                    senha_pix = input("Digite sua senha:")  
+                    if senha_pix == senha:
+                        saldo = saldo - valor_pix
+                    else:
+                        print("Senha incorreta!")
+                elif escolha_principal == 4:#Se o usuário escolher visualizar 
+                    senha_extrato = input("Digite sua senha!")
+                    if senha_extrato == senha:
+                        print("Extrato: ", saldo)
+                    else:
+                        print("Senha incorreta!")
+                elif escolha_principal == 5:#Encerrar
+                    senha_encerrar = input("Digite sua senha!")
+                    if senha_encerrar == senha:
+                        break
+                    else:
+                        print("Senha incorreta!")
         else:
             print("Usuário ou senha incorretos!")
+
