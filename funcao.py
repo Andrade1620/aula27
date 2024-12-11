6377359472f0ae03c0dae67c3cf2f154e930d436
Calebe <calebe.correia@aluno.senai.br>
2024-12-11 10:01:09 -0500

    Alterações salvas das funções às 10:00 da manhã, dia 11/12/2024

--- funcao.py
+++ funcao.py
@@ -1,13 +1,16 @@
-#DECLARAR UMA FUNÇÃO
-def bom_dia():
-    print("Bom dia!")
+#DECLARANDO UMA FUNÇÃO
+def saudacoes(hora_do_dia):#Exibe a saudação correspondente ao horário do dia
+    #Condição para dar BOM DIA!
+    if (hora_do_dia >= 0) and (hora_do_dia <= 12):
+        print("Bom dia!!!!")
+    #Condição para dar BOA TARDE!
+    elif (hora_do_dia >=13) and (hora_do_dia <=18):
+        print("Boa tarde!!!!!")
+    #Condição para dar Boa noite
+    elif (hora_do_dia >=19) and (hora_do_dia <=23):
+        print("Boa noite!!!!!")
+#FORA DA FUNÇÃO
+#peço para o usuário dizer a hora atual
+resposta = float(input("Digite que horas são: \n"))
 
-def boa_noite():
-    print("Boa noite!")
-
-def boa_tarde():
-    print("Boa tarde!")
-
-bom_dia()
-boa_noite()
-boa_tarde()
+saudacoes(resposta)
