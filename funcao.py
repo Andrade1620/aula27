9c52b3f0a90348620506db1246fa169f05aae05b
Calebe <calebe.correia@aluno.senai.br>
2024-12-11 08:46:47 -0500

    Alterações das funções salvas!

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#DECLARAR UMA FUNÇÃO
+def bom_dia():
+    print("Bom dia!")
+
+def boa_noite():
+    print("Boa noite!")
+
+def boa_tarde():
+    print("Boa tarde!")
+
+bom_dia()
+boa_noite()
+boa_tarde()
